643a4ff446be90336cc21abbb2a009ac16dcdbd9
ravi1998h <ravigoudra14@gmail.com>
2021-07-15 20:10:16 -1200

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -5,7 +5,7 @@ import datetime
 # Create your models here.
 # models.py
 class Employee(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=99)
     email = models.EmailField()
     contact = models.CharField(max_length=15)
     date = models.DateField("Date", default=datetime.date.today)
